035f3490ec2042a67d9b458a6be5452c56ea8644
Martin Njau <njau_martin@yahoo.comgit config --global user.email njau_martin@yahoo.comclearclear>
2023-10-08 18:59:12 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  
+  if(distance<=400){
+    return 'This one is on me!';
+  }
+  else if(distance>2000&&distance<=2500){
+    return 'I will gladly take your thirty bucks.';
+  }
+  
+  else{
+    return 'No can do.';
+  }
+  
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(location){
+ return location==='NYC'?'Ok, sounds good.' :'No go.';
+  
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(Tip){
+ 
+  switch(Tip){
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+      
+
+      case 'not as generous':
+        return 'Thank you.';
+        break;
+
+         default:
+         return 'Bye.';
+  }
+  
 }
